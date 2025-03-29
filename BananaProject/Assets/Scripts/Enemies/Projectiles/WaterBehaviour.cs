1082454888c0fbc506f26eef683bfe8f22bbe319
Sethiepooh <sethwalters2005@outlook.com>
2025-03-29 16:30:40 -0700

    update

--- BananaProject/Assets/Scripts/Enemies/Projectiles/WaterBehaviour.cs
+++ BananaProject/Assets/Scripts/Enemies/Projectiles/WaterBehaviour.cs
@@ -46,18 +46,5 @@ public class WaterBehaviour : MonoBehaviour
         }  
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-            Vector3 player = collision.gameObject.transform.position;
-            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-
-            Vector3 pushForce = this.transform.parent.position - player;
-            pushForce.Normalize();
-            pushForce.x = pushForce.x * 1000;
-
-            rb.AddForce(-pushForce, ForceMode2D.Force);
-        }
-    }
+    
 }
